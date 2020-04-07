a44dcd8190c4d89c7a2245cb4cb4677e3f4fbbea
Saurabh kacholiya <kacholiyasaurabh.com>
2020-04-07 10:04:21 +0530

    install dependencies and remove component folder

--- prabhagApp/LoginScreen/Login.js
+++ prabhagApp/LoginScreen/Login.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { View,Text,Button } from "react-native";
+import { 
+    View,
+    Text,
+    Button,
+    SafeAreaView,
+} from "react-native";
+import { styles } from "./style";
 
 function Login({navigation}){
     return (
-        <View>
+        <View style={styles.container}>
             <Text>go to search screen</Text>
             <Button title="heh" onPress={() => navigation.navigate('Home')}/>
         </View>
--- prabhagApp/config/routes.js
+++ prabhagApp/config/routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Main from "../components/main/main";
-import Login from "../components/LoginScreen/Login";
+import Main from "../main/main";
+import Login from "../LoginScreen/Login";
 
 const MainStack = createStackNavigator();
 
