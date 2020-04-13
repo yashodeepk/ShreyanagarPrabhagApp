0ab20ed5c7bc8094e743b6062458473515e02c65
Saurabh kacholiya <kacholiyasaurabh.com>
2020-04-13 07:33:15 +0530

    add search design

--- prabhagApp/SearchScreen/index.js
+++ prabhagApp/SearchScreen/index.js
@@ -18,12 +18,14 @@ import injectReducer from '../utils/injectReducer';
 import Loader from '../utils/Loader';
 import reducer from './reducer';
 import saga from './saga';
-import { 
+import {
   FontAwesome,
   Entypo,
 } from 'react-native-vector-icons';
+import { searchData } from "../dummyData";
+import { FlatList } from "react-native-gesture-handler";
 
-function logout(){
+function logout() {
   Alert.alert(
     'Logout',
     'Are you sure that you want to logout?',
@@ -45,75 +47,82 @@ function SearchScreen({
     <SafeAreaView style={styles.safeAreaViewStyle}>
       <View style={styles.headerView}>
         <View style={styles.logoutView}>
-        <Entypo 
-          style={styles.iconStyle} 
-          name="log-out" 
-          color={"#fff"} 
-          size={20} 
-          onPress={logout}
-        />
+          <Entypo
+            style={styles.iconStyle}
+            name="log-out"
+            color={"#fff"}
+            size={20}
+            onPress={logout}
+          />
         </View>
         <View style={styles.searchView}>
           <View style={styles.searchViewWithElement}>
-            <FontAwesome 
-              style={styles.iconStyle} 
-              name="search" 
-              color={"#F7882F"} 
-              size={16} 
+            <FontAwesome
+              style={styles.iconStyle}
+              name="search"
+              color={"#F7882F"}
+              size={16}
             />
-            <TextInput 
-              style={styles.textInputStyle} 
-              placeholder="Search by name, occupation, blood-group" 
+            <TextInput
+              style={styles.textInputStyle}
+              placeholder="Search by name, occupation, blood-group"
             />
           </View>
         </View>
+      </View>
+      <View style={styles.container}>
+        <FlatList 
+          
+        />
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#fff',
   },
   safeAreaViewStyle: {
+    flex: 1,
     backgroundColor: '#F7882F',
   },
-  headerView:{
-    height:90,
+  headerView: {
+    height: 90,
     backgroundColor: '#F7882F',
-    flex:0,
-    justifyContent:'flex-end',
+    flex: 0,
+    justifyContent: 'flex-end',
   },
-  logoutView:{
-    height:40,
-    backgroundColor:'#F7882F',
-    alignItems:'flex-end',
-    justifyContent:'center',
-    marginRight:5,
+  logoutView: {
+    height: 40,
+    backgroundColor: '#F7882F',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginRight: 5,
   },
-  searchView:{
+  searchView: {
     backgroundColor: '#fff',
-    height:40,
-    marginTop:0,
-    margin:10,
-    borderRadius:5,
+    height: 40,
+    marginTop: 0,
+    margin: 10,
+    borderRadius: 5,
   },
-  iconStyle:{
-    marginRight:10,
-    marginLeft:10,
+  iconStyle: {
+    marginRight: 10,
+    marginLeft: 10,
   },
-  searchViewWithElement:{
-    flex:1,
-    flexDirection:'row',
-    alignItems:'center',
+  searchViewWithElement: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  textInputStyle:{
+  textInputStyle: {
     fontSize: 16,
-    fontWeight:'bold',
-    flex:1,
+    fontWeight: 'bold',
+    flex: 1,
   },
 });
 
---
+++ prabhagApp/dummyData.js
@@ -0,0 +1,43 @@
+export const searchData = [
+    {
+        "id":"1",
+        "name":"Yashodeep Ramesh Kacholiya",
+        "bloodgroup":"B+",
+        "occupation":"Software Engineer",
+        "mobileno":8275861835,
+        "gender": 'male',
+    },
+    {
+        "id":"2",
+        "name":"Yashodeep Ramesh Kacholiya",
+        "bloodgroup":"B+",
+        "occupation":"Software Engineer",
+        "mobileno":8275861835,
+        "gender": 'female',
+    },
+    {
+        "id":"3",
+        "name":"Yashodeep Ramesh Kacholiya",
+        "bloodgroup":"B+",
+        "occupation":"Software Engineer",
+        "mobileno":8275861835,
+        "gender": 'male',
+    },
+    {
+        "id":"41",
+        "name":"Yashodeep Ramesh Kacholiya",
+        "bloodgroup":"B+",
+        "occupation":"Software Engineer",
+        "mobileno":8275861835,
+        "gender": 'female',
+    },
+    {
+        "id":"5",
+        "name":"Yashodeep Ramesh Kacholiya",
+        "bloodgroup":"B+",
+        "occupation":"Software Engineer",
+        "mobileno":8275861835,
+        "gender": 'male',
+    },
+
+]
