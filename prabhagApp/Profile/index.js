8811ae41760d732efe7ed197fe8d424ae14dce68
Saurabh Kacholiya <saurabhkacholiya@MacBook-Air.local>
2020-06-07 21:38:29 +0530

    show user based image

--- prabhagApp/Profile/index.js
+++ prabhagApp/Profile/index.js
@@ -3,8 +3,12 @@ import {
  View,
  Text,
  StyleSheet,
+ Image,
 } from "react-native";
 import { getLoginDetails } from "../utils/asyncStorage";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import womenImg from '../assets/womenImg.png'
+import menImg from '../assets/menImg.png'
 
 function Profile(){
     const [userData , setUserData] = useState(null)
@@ -26,18 +30,39 @@ function Profile(){
     }
 
     return (
-        <View style={styles.center}>
-            <Text>EDIT YOUR PROFILE</Text>
-        </View>
+        <SafeAreaView style={styles.flexOne}>
+            <View style={{flex:1,alignItems:'center'}}>
+                <Image
+                    style={styles.userImage}
+                    source={
+                        userData.Gender === "Male"
+                        ? menImg
+                        : womenImg
+                    }
+                />
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    flexOne:{
+        flex:1,
+        backgroundColor:'#fff'
+    },
     center:{
         flex:1,
         justifyContent:'center',
         alignItems: 'center',
     },
+    userImage : {
+        margin:10,
+        height: 100, 
+        width: 100, 
+        borderRadius: 100,
+        borderWidth: 0.5,
+        borderColor:'black',
+    }
 })
 
 export default Profile
